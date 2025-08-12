818bb7a263fde0bf0c5deb16e64c63b33b03841e
Sithum Sathsara <89584440+SithumSathsaras@users.noreply.github.com>
2025-08-12 20:02:45 +0530

    fix

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, jsonify
-from api.scheduled_call import _actual_handler_logic
+from flask import Flask, jsonify, render_template, url_for
+import os
+from scheduled_call import _actual_handler_logic
 
-app = Flask(__name__)
+# Configure Flask to find templates and static files in parent directories
+app = Flask(__name__, 
+            template_folder='../templates',
+            static_folder='../static')
 
 @app.route('/api/scheduled-call', methods=['GET'])
 def scheduled_call():
@@ -9,6 +13,12 @@ def scheduled_call():
     response = jsonify(response_data.get("body"))
     response.status_code = response_data.get("statusCode")
     return response
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Add any other routes for your application
 
 if __name__ == '__main__':
     app.run(debug=True)
