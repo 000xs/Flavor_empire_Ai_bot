56b7b67028ac123ebab7056adf46102d172f4e32
Sithum Sathsara <89584440+SithumSathsaras@users.noreply.github.com>
2025-08-12 16:54:53 +0530

    flask app

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+from api.scheduled_call import _actual_handler_logic
+
+app = Flask(__name__)
+
+@app.route('/api/scheduled-call', methods=['GET'])
+def scheduled_call():
+    response_data = _actual_handler_logic()
+    response = jsonify(response_data.get("body"))
+    response.status_code = response_data.get("statusCode")
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
